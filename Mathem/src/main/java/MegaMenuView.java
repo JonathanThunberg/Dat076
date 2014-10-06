7eaa0f4039a2fb46dd6726b78a10b0883e568424
Patrik Koskenniemi <patrik_koskenniemi@hotmail.com>
2014-10-06 15:45:14 +0200

    Megamenu created

---
+++ Mathem/src/main/java/MegaMenuView.java
@@ -0,0 +1,15 @@
+import javax.faces.bean.ManagedBean;
+ 
+@ManagedBean
+public class MegaMenuView {
+  
+    private String orientation = "horizontal";
+ 
+    public String getOrientation() {
+        return orientation;
+    }
+ 
+    public void setOrientation(String orientation) {
+        this.orientation = orientation;
+    }
+}
