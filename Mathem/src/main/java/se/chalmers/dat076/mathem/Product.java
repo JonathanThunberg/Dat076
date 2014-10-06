1ea728ae962b1b51d24f1415a4acee30187b5162
Jonathan Thunberg <vikingaskor@hotmail.com>
2014-10-06 10:46:45 +0200

    Added a java file

---
+++ Mathem/src/main/java/se/chalmers/dat076/mathem/Product.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package se.chalmers.dat076.mathem;
+
+/**
+ *
+ * @author tuna
+ */
+public class Product {
+    
+}
