8c92499ce66c594967be7b67ad9195176c57aade
Victor <noordhan@gmail.com>
2014-10-13 13:57:21 +0200

    Implemented a table for products

---
+++ Mathem/src/main/java/se/chalmers/dat076/mathem/view/DataGridView.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package se.chalmers.dat076.mathem.view;
+
+/**
+ *
+ * @author victor_nordh92
+ */
+ /*
+import java.io.Serializable;
+import java.util.List;
+import java.util.logging.Logger;
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.ViewScoped;
+import javax.inject.Inject;
+ 
+@ManagedBean
+@ViewScoped
+public class DataGridView implements Serializable {
+     
+    private List<Product> prods;
+    //private static final Logger LOG = Logger.getLogger(Bönan.class.getName());
+    private Product selectedProd;
+     
+    @ManagedProperty("#{productService}")
+    private ProductService service;
+     
+    @PostConstruct
+    public void init() {        
+    }
+    
+    protected DataGridView() {
+        // Must have for CDI
+    }
+
+    @Inject
+    public DataGridView(List<Product> prod) {
+        this.prod = prod;
+    }
+
+    
+    public List<Product> getProd() {
+        return prods;
+    }
+ 
+    public void setService(ProductService service) {
+        this.service = service;
+    }
+ 
+    public Product getSelectedProd() {
+        return selectedProd;
+    }
+ 
+    public void setSelectedProd(Product selectedCar) {
+        this.selectedProd = selectedProd;
+    }
+}*/
---
+++ Mathem/src/main/java/se/chalmers/dat076/mathem/view/ProductService.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package se.chalmers.dat076.mathem.view;
+
+/**
+ *
+ * @author victor_nordh92
+ */
+ /*
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import javax.faces.bean.ApplicationScoped;
+import javax.faces.bean.ManagedBean;
+ 
+@ManagedBean(name = "ProductService")
+@ApplicationScoped
+public class ProductService {
+     
+     
+    public List<Product> createProductList(int size) {
+        List<Product> list = new ArrayList<Product>();
+        for(int i = 0 ; i < size ; i++) {
+            list.add(new Product(id,name,price));
+        }
+         
+        return list;
+    }
+ 
+    public List<String> getPrice() {
+        return Arrays.asList(price);
+    }
+     
+    public List<String> getName() {
+        return Arrays.asList(name);
+    }
+}
+*/
