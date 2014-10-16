da66680c29b76badb9fd1c10849f365e4f336e42
Julia Friberg <juliafriberg93@gmail.com>
2014-10-16 11:48:39 +0200

    Shopping cart dialog works

--- Mathem/src/main/java/se/chalmers/dat076/mathem/view/ShoppingCartDialog.java
+++ Mathem/src/main/java/se/chalmers/dat076/mathem/view/ShoppingCartDialog.java
@@ -16,7 +16,9 @@ import org.primefaces.context.RequestContext;
 public class ShoppingCartDialog {
     
     public void openCart() {
+        
         RequestContext.getCurrentInstance().openDialog("shoppingcart.xhtml");
+        System.out.println("open cart");
     }
 
 }
