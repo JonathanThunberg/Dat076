508abd6820a988bbf6610ec3e4540262ad41a66b
Victor <noordhan@gmail.com>
2014-10-19 14:12:06 +0200

    Created a About Us page

--- Mathem/src/main/java/se/chalmers/dat076/mathem/view/CustomerRegistryBB.java
+++ Mathem/src/main/java/se/chalmers/dat076/mathem/view/CustomerRegistryBB.java
@@ -6,6 +6,8 @@
 package se.chalmers.dat076.mathem.view;
  
 import javax.faces.bean.ManagedBean;
+import se.chalmers.dat076.mathem.model.entityclasses.Adresses;
+import se.chalmers.dat076.mathem.model.entityclasses.Customer;
 /**
  *
  * @author victor_nordh92
@@ -13,12 +15,41 @@ import javax.faces.bean.ManagedBean;
 @ManagedBean
 public class CustomerRegistryBB {
    
-    private String text;
+    private Customer customer;
+    private String name = customer.getName();
+    private Integer phone = customer.getPhone();
+    private String email = customer.getEmail();
+    private Adresses adress = customer.getAdresses();
  
-    public String getText() {
-        return text;
+    public String getName() {
+        return name;
     }
-    public void setText(String text) {
-        this.text = text;
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public Integer getPhone() {
+        return phone;
+    }
+    
+    public void setPhone(Integer phone) {
+        this.phone = phone;
+    }
+    
+    public String getEmail() {
+        return email;
+    }
+        
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    public Adresses getAdress() {
+        return adress;
+    }
+        
+    public void setEmail(Adresses adress) {
+        this.adress = adress;
     }
 }
