cb4b21b74e47a073131ba5ffcde34e1d72bf6b9f
Julia Friberg <juliafriberg93@gmail.com>
2014-10-24 13:01:39 +0200

    Changes in my page

--- Mathem/src/main/java/se/chalmers/dat076/mathem/ctrl/MyPageCtrl.java
+++ Mathem/src/main/java/se/chalmers/dat076/mathem/ctrl/MyPageCtrl.java
@@ -41,23 +41,30 @@ public class MyPageCtrl {
         FacesContext context = FacesContext.getCurrentInstance();
         if(PasswordUtil.PasswordToHash(regBB.getPassword()).equals(shop.getUserCatalogue().getByKey(regBB.getUsername()).get(0).getPassword())){
             Customer customer = new Customer(regBB.getUsername());
-            if(!regBB.getNewPassword().equals("")){
+            boolean adressChanged = !shop.getCustomerCatalogue().getByKey(regBB.getUsername()).get(0).getAdresses().getAdressesPK().equals(new AdressesPK(regBB.getStreetname(),regBB.getCity()));
+            boolean passwordChanged = !regBB.getNewPassword().equals("");
+            if(passwordChanged){
                 User user = new User(regBB.getUsername());
                 user.setPassword(PasswordUtil.PasswordToHash(regBB.getNewPassword()));
+                shop.getCustomerCatalogue().delete(customer.getUsername());
                 shop.getUserCatalogue().delete(user.getUsername());
                 shop.getUserCatalogue().create(user);
+                customer.setUsers(user);
             }
             
-            if(!shop.getCustomerCatalogue().getByKey(regBB.getUsername()).get(0).getAdresses().getAdressesPK().equals(new AdressesPK(regBB.getStreetname(),regBB.getCity()))) {
+            if(adressChanged) {
                 if(shop.getAdressCatalogue().getByKey(new AdressesPK(regBB.getStreetname(), regBB.getCity())).isEmpty()) {
                     Adress adress = new Adress(regBB.getStreetname(), regBB.getCity());
                     adress.setPostalcode(regBB.getPostcode());
                     
-                    shop.getCustomerCatalogue().delete(customer.getUsername());
+                    if(!passwordChanged) {
+                        shop.getCustomerCatalogue().delete(customer.getUsername());
+                    }
                     shop.getAdressCatalogue().delete(new AdressesPK(regBB.getStreetname(), regBB.getCity()));
                     shop.getAdressCatalogue().create(adress);
                     customer.setAdresses(adress);
                     updateCustomer(customer);
+                    
                 }else{
                     context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN, "Adress upptagen", ""));
                     context.getExternalContext().getFlash().setKeepMessages(true);
@@ -73,7 +80,6 @@ public class MyPageCtrl {
                 shop.getAdressCatalogue().create(adress);
                 customer.setAdresses(adress);
                 updateCustomer(customer);
-                
             }
         }else{
             context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN, "Fel lösenord", ""));
