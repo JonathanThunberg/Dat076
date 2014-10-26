ca1ed30922d7d5046737aeeda710d3cbfc613273
Victor <noordhan@gmail.com>
2014-10-26 18:31:43 +0100

    change the product page

--- Mathem/src/main/java/se/chalmers/dat076/mathem/ctrl/SearchCtrl.java
+++ Mathem/src/main/java/se/chalmers/dat076/mathem/ctrl/SearchCtrl.java
@@ -11,7 +11,9 @@ import javax.enterprise.context.RequestScoped;
 import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
+import se.chalmers.dat076.mathem.model.OrderItem;
 import se.chalmers.dat076.mathem.model.Shop;
+import se.chalmers.dat076.mathem.model.ShoppingCart;
 import se.chalmers.dat076.mathem.model.entityclasses.Product;
 import se.chalmers.dat076.mathem.view.SearchBB;
 
@@ -28,20 +30,14 @@ public class SearchCtrl{
     private SearchBB sBB;
     
     @Inject
+    private ShoppingCart cart;
+    
+    @Inject
     public void setSearchBB(SearchBB sBB) {
         this.sBB = sBB;
     }
-    
-    public void search() throws IOException{
-        System.out.println("Nu är vi i search");
-        List<Product> p = shop.getProductCatalogue().getByName(sBB.getProductname());
-        
-        if(p.isEmpty()){
-            System.out.println("Nu är det tomt");            
-        }
-        else{
-            System.out.println("Produkten p.get =  "+ p.get(0).getName());
-            FacesContext.getCurrentInstance().getExternalContext().getRequestMap().put("searchitem", p.get(0).getId());          
-        }
+   
+    public void buy(OrderItem product) {
+        cart.add(product);
     }
 }
